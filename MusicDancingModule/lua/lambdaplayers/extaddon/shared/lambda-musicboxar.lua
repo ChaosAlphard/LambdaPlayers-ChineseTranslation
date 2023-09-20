14cd9973dd497931a4ca43725096afc8394b2bb9
Iris <Iris6400@hotmail.com>
2023-09-20 18:33:59 +0800

    修复"Tried to use a NULL IGModAudioChannel"问题

--- MusicDancingModule/lua/lambdaplayers/extaddon/shared/lambda-musicboxar.lua
+++ MusicDancingModule/lua/lambdaplayers/extaddon/shared/lambda-musicboxar.lua
@@ -1,8 +1,8 @@
 
 CreateLambdaConvar( "lambdaplayers_musicbox_dancechance", 10, true, false, false, "Lambda Player 在音乐盒附近跳舞的几率", 0, 100, { type = "Slider", decimals = 0, name = "跳舞几率", category = "Music Box" } )
-CreateLambdaConvar( "lambdaplayers_musicbox_drawvisualizer", 1, true, true, false, "在音乐盒上方绘制可视化音频特效", 0, 1, { type = "Bool", name = "音频可视化", category = "Music Box" } )
-CreateLambdaConvar( "lambdaplayers_musicbox_drawvisualizerlight", 1, true, true, false, "If the Music Visualizer should draw a light according to the music", 0, 1, { type = "Bool", name = "Draw Light", category = "Music Box" } )
-CreateLambdaConvar( "lambdaplayers_musicbox_visualizerresolution", 100, true, true, false, "可视化音频特效", 20, 200, { type = "Slider", decimals = 0, name = "音频可视化分辨率", category = "Music Box" } )
+CreateLambdaConvar( "lambdaplayers_musicbox_drawvisualizer", 1, true, true, false, "在音乐盒上方绘制圆形音频频谱", 0, 1, { type = "Bool", name = "音频可视化", category = "Music Box" } )
+CreateLambdaConvar( "lambdaplayers_musicbox_drawvisualizerlight", 1, true, true, false, "绘制音频频谱时使音乐盒发光", 0, 1, { type = "Bool", name = "音乐盒发光", category = "Music Box" } )
+CreateLambdaConvar( "lambdaplayers_musicbox_visualizerresolution", 100, true, true, false, "圆形音频频谱柱体的数量", 20, 200, { type = "Slider", decimals = 0, name = "音频可视化分辨率", category = "Music Box" } )
 CreateLambdaConvar( "lambdaplayers_musicbox_samples", 5, true, true, false, "音频可视化的采样等级(插件作者推荐的值为5)", 0, 7, { type = "Slider", decimals = 0, name = "采样等级", category = "Music Box" } )
 CreateLambdaConvar( "lambdaplayers_musicbox_shufflemusic", 1, true, true, true, "随机化音乐播放顺序", 0, 1, { type = "Bool", name = "随机播放", category = "Music Box" } )
 CreateLambdaConvar( "lambdaplayers_musicbox_custommusiconly", 0, true, false, false, "仅播放自定义音乐", 0, 1, { type = "Bool", name = "仅自定义音乐", category = "Music Box" } )
